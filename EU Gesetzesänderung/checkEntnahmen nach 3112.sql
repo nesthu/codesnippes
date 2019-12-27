7a29dd9ab2d494a0f203948882772accd48890f4
nesthu <nest_hu@gmx.de>
2019-12-27 11:26:18 +0100

    view angepasst so das nur Entnahmen von im Fokus befindlichen Teilen gezeigt werden und S und H wird beachtet bei Mengenberechnung

--- EU Gesetzesänderung/checkEntnahmen nach 3112.sql
+++ EU Gesetzesänderung/checkEntnahmen nach 3112.sql
@@ -1,5 +1,19 @@
-select materialnummer, sum(Menge)
-from MM_MSEG_MATBELEG_12M_DRS_VIEW
-where sonderbestandskennzeichen = 'K' and bewegungsart in ('261','201') and buchungsdatum_beleg > '31.12.18'
-group by materialnummer
+select  -- Select zum Auflisten Materialnummer und Summe Veräuche ab dem 1.1.19 Stornierungen werden berücksichtigt
+list.materialnummer,
+sum(list.Menge)
+from (
+        select
+        materialnummer,
+        case
+            when SOLL_HABEN_KENNZEICHEN = 'S' then 0 - MENGE
+            when SOLL_HABEN_KENNZEICHEN = 'H' then Menge
+            else 0
+            end as MENGE,
+        SOLL_HABEN_KENNZEICHEN
+        from MM_MSEG_MATBELEG_12M_DRS_VIEW ms
+        inner join CHECK_MAT_EU_STEUER_CHANGE c on c.MATERIAL = ms.materialnummer
+        where sonderbestandskennzeichen = 'K' and bewegungsart in ('261','201','262','502','202') and buchungsdatum_beleg > '25.12.19'  -- Datum muss auf 31.12.19 angepasster werden !! um Entnahmen ab 1.1. zu sehen
+) list
+
+group by list.materialnummer;
 -- Bewegungsarten noch prüfen  mit Tableau checken Jahresverbrauch .. Tableau soll zu dieser Auswertung passen für die Teile dann perfekt
