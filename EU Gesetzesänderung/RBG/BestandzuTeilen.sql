efa42d7beb0cd6d9abeb8f7ce827362beb03a95f
nesthu <nest_hu@gmx.de>
2020-01-22 12:40:16 +0100

    update welcher funktioniert

--- EU Gesetzesänderung/RBG/BestandzuTeilen.sql
+++ EU Gesetzesänderung/RBG/BestandzuTeilen.sql
@@ -12,3 +12,24 @@ from CHECK_MAT_EU_STEUER_RBGCN lr
                 from MM_BESTAND_HISTORIE_ALLE_WERKE
                 where standort = 'RBG' and last_update like '%01.01.19%'
                 ) vb on vb.material = lr.Material
+--old version
+
+-- neue Version als view checkmp_Temp für Exel
+select
+material,
+Standort,
+konsibestand,
+konsi_gesperrt,
+DATUM
+from (
+
+select
+material,
+standort,
+konsibestand,
+konsi_gesperrt,
+CAST(last_update AS DATE) as DATUM
+from MM_BESTAND_HISTORIE_ALLE_WERKE
+where standort = 'RBG'
+)
+where DATUM like '%01.01.20%'
